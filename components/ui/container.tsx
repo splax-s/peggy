1cbeb1f753b269baa6bf562cbd90bac3710a893f
Ikenga <65579869+KING-BENGZ@users.noreply.github.com>
2023-11-11 22:53:01 +0000

    Delete components directory

--- components/ui/container.tsx
+++
@@ -1,9 +0,0 @@
-interface ContainerProps {
-  children: React.ReactNode;
-}
-
-const Container: React.FC<ContainerProps> = ({ children }) => {
-  return <div className="mx-auto">{children}</div>;
-};
-
-export default Container;
